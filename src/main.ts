0e5d0f49556627777f3855763c3de3bcb9935265
wujingquan <i@wujingquan.com>
2022-07-09 01:00:06 +0800

    add common filter

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 异常过滤器
+  // 这里一定要注意引入自定义异常的先后顺序，不然异常捕获逻辑会出现混乱。
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
