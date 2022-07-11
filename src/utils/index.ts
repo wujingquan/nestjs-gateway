6e8a04d7e84debad23afab89a406fa1acd6a231e
wujingquan <i@wujingquan.com>
2022-07-12 00:22:22 +0800

    getConfig支持类型获取

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,10 +8,10 @@ export const getEnv = () => {
 };
 
 // 读取项目配置
-export const getConfig = () => {
+export const getConfig = (type?: string) => {
   const environment = getEnv();
   const yamlPath = path.join(process.cwd(), `./.config/.${environment}.yaml`);
   const file = fs.readFileSync(yamlPath, 'utf8');
   const config = parse(file);
-  return config;
+  return type ? config[type] : config;
 };
